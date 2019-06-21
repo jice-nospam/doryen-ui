a2554037d9f824689ecf94a152db438049157d7d
jice <jice.nospam@gmail.com>
2019-06-21 18:29:35 +0200

    base widgets

---
+++ examples/showcase.rs
@@ -0,0 +1,125 @@
+extern crate doryen_rs;
+extern crate doryen_ui;
+
+use doryen_rs::{App, AppOptions, Color, DoryenApi, Engine, TextAlign};
+use doryen_ui as ui;
+
+const CONSOLE_WIDTH: u32 = 80;
+const CONSOLE_HEIGHT: u32 = 50;
+
+#[derive(Default)]
+struct Showcase {
+    ctx: ui::Context,
+    status: String,
+}
+
+impl Showcase {
+    pub fn new() -> Self {
+        Default::default()
+    }
+    fn update_input_data(&mut self, api: &mut dyn DoryenApi) {
+        let input = api.input();
+        let (mx, my) = input.mouse_pos();
+        let mpos = ui::Pos {
+            x: mx as i32,
+            y: my as i32,
+        };
+        self.ctx.input_mouse_pos(mpos);
+        if input.mouse_button_pressed(0) {
+            self.ctx.input_mouse_down(1);
+        } else if input.mouse_button_released(0) {
+            self.ctx.input_mouse_up(1);
+        }
+    }
+    fn build_ui(&mut self) {
+        self.ctx.begin();
+        self.ctx.frame_start("buttons", 15, 4);
+        if self.ctx.button("1", ui::TextAlign::Center) {
+            self.status = "button 1 pressed!".to_owned();
+        }
+        if self.ctx.button("2", ui::TextAlign::Center) {
+            self.status = "button 2 pressed!".to_owned();
+        }
+        self.ctx.frame_end();
+        self.ctx.label(&self.status, ui::TextAlign::Left);
+        self.ctx.end();
+    }
+    fn render_rect(&self, api: &mut dyn DoryenApi, r: &ui::Rect, col: Color) {
+        api.con()
+            .area(r.x, r.y, r.w as u32, r.h as u32, None, Some(col), None);
+    }
+    fn render_text(&self, api: &mut dyn DoryenApi, x: i32, y: i32, txt: &str, col: Color) {
+        api.con().print(x, y, txt, TextAlign::Left, Some(col), None);
+    }
+    fn render_frame(&self, api: &mut dyn DoryenApi, txt: &str, r: &ui::Rect, col: Color) {
+        let con = api.con();
+        let txtcol = conv_color(ui::ColorCode::Text);
+        con.rectangle(
+            r.x,
+            r.y,
+            r.w as u32,
+            r.h as u32,
+            Some(txtcol),
+            Some(col),
+            None,
+        );
+        con.print(
+            r.x + r.w / 2,
+            r.y,
+            txt,
+            TextAlign::Center,
+            Some(txtcol),
+            None,
+        );
+    }
+}
+
+impl Engine for Showcase {
+    fn init(&mut self, _api: &mut dyn DoryenApi) {}
+    fn update(&mut self, api: &mut dyn DoryenApi) {
+        self.update_input_data(api);
+        self.build_ui();
+    }
+    fn render(&mut self, api: &mut dyn DoryenApi) {
+        for c in self.ctx.get_render_commands() {
+            match c {
+                ui::Command::Rect(r, col) => self.render_rect(api, &r, conv_color(col)),
+                ui::Command::Text(txt, pos, col) => {
+                    self.render_text(api, pos.x, pos.y, &txt, conv_color(col))
+                }
+                ui::Command::Frame(txt, r, col) => {
+                    self.render_frame(api, &txt, &r, conv_color(col))
+                }
+            }
+        }
+    }
+    fn resize(&mut self, _api: &mut dyn DoryenApi) {}
+}
+
+fn conv_color(c: ui::ColorCode) -> Color {
+    match c {
+        ui::ColorCode::Background => (10, 10, 20, 255),
+        ui::ColorCode::Foreground => (200, 200, 255, 255),
+        ui::ColorCode::ButtonBackground => (50, 60, 70, 255),
+        ui::ColorCode::ButtonBackgroundFocus => (100, 130, 170, 255),
+        ui::ColorCode::ButtonBackgroundHover => (100, 120, 160, 255),
+        ui::ColorCode::Text => (200, 220, 250, 255),
+    }
+}
+
+fn main() {
+    let mut app = App::new(AppOptions {
+        console_width: CONSOLE_WIDTH,
+        console_height: CONSOLE_HEIGHT,
+        screen_width: CONSOLE_WIDTH * 8,
+        screen_height: CONSOLE_HEIGHT * 8,
+        window_title: "doryen-ui showcase".to_owned(),
+        font_path: "terminal_8x8.png".to_owned(),
+        vsync: true,
+        fullscreen: false,
+        show_cursor: true,
+        resizable: true,
+    });
+    app.set_engine(Box::new(Showcase::new()));
+    app.run();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,335 @@
+#[derive(Clone, Copy)]
+pub enum ColorCode {
+    Background,
+    Foreground,
+    ButtonBackground,
+    ButtonBackgroundHover,
+    ButtonBackgroundFocus,
+    Text,
+}
 
+pub enum TextAlign {
+    Left,
+    Center,
+    Right,
+}
+
+pub type Coord = i32;
+
+#[derive(Clone, Copy, Default, Debug)]
+pub struct Pos {
+    pub x: Coord,
+    pub y: Coord,
+}
+
+impl From<&Rect> for Pos {
+    fn from(r: &Rect) -> Self {
+        Self { x: r.x, y: r.y }
+    }
+}
+
+#[derive(Default, Clone, Debug)]
+pub struct Rect {
+    pub x: Coord,
+    pub y: Coord,
+    pub w: Coord,
+    pub h: Coord,
+}
+
+impl Rect {
+    pub fn new(x: Coord, y: Coord, w: Coord, h: Coord) -> Self {
+        Self { x, y, w, h }
+    }
+    pub fn contains(&self, p: Pos) -> bool {
+        p.x >= self.x && p.y >= self.y && p.x < self.x + self.w && p.y < self.y + self.h
+    }
+}
+
+pub enum Command {
+    Rect(Rect, ColorCode),
+    Text(String, Pos, ColorCode),
+    Frame(String, Rect, ColorCode),
+}
+
+pub trait Renderer {
+    fn rectangle(&mut self, rect: &Rect, col: ColorCode);
+    fn text(&mut self, pos: Pos, txt: &str, col: ColorCode);
+    fn frame(&mut self, txt: &str, rect: &Rect, col: ColorCode);
+}
+
+pub const MOUSE_BUTTON_LEFT: usize = 1;
+pub const MOUSE_BUTTON_RIGHT: usize = 2;
+pub const MOUSE_BUTTON_MIDDLE: usize = 4;
+
+pub enum Layout {
+    Vbox(Coord),
+    Hbox(Coord),
+}
+
+#[derive(Default)]
+pub struct Context {
+    id: usize,
+    focus: usize,
+    hover: usize,
+    updated_focus: bool,
+    mouse_pos: Pos,
+    last_mouse_pos: Pos,
+    mouse_delta: Pos,
+    mouse_pressed: usize,
+    mouse_down: usize,
+    commands: Vec<Command>,
+    layouts: Vec<Layout>,
+    cursor: Pos,
+}
+
+impl Context {
+    pub fn new() -> Self {
+        Self {
+            mouse_pos: Pos { x: -1, y: -1 },
+            ..Default::default()
+        }
+    }
+
+    pub fn input_mouse_pos(&mut self, pos: Pos) {
+        self.mouse_pos = pos;
+    }
+    pub fn input_mouse_down(&mut self, button: usize) {
+        self.mouse_down |= button;
+        self.mouse_pressed |= button;
+    }
+    pub fn input_mouse_up(&mut self, button: usize) {
+        self.mouse_down &= !button;
+    }
+
+    pub fn begin(&mut self) {
+        self.mouse_delta.x = self.mouse_pos.x - self.last_mouse_pos.x;
+        self.mouse_delta.y = self.mouse_pos.y - self.last_mouse_pos.y;
+        self.cursor.x = 0;
+        self.cursor.y = 0;
+    }
+    pub fn end(&mut self) {
+        self.mouse_pressed = 0;
+        self.last_mouse_pos = self.mouse_pos;
+    }
+    pub fn render(&mut self, renderer: &mut impl Renderer) {
+        for c in self.commands.iter() {
+            match c {
+                Command::Rect(r, col) => renderer.rectangle(r, *col),
+                Command::Text(txt, pos, col) => renderer.text(*pos, txt, *col),
+                Command::Frame(txt, r, col) => renderer.frame(txt, r, *col),
+            }
+        }
+        self.commands.clear();
+    }
+    pub fn get_render_commands(&mut self) -> Vec<Command> {
+        let mut v = Vec::new();
+        v.append(&mut self.commands);
+        self.commands.clear();
+        v
+    }
+
+    fn next_id(&mut self) -> usize {
+        self.id += 1;
+        self.id
+    }
+    pub fn vbox_start(&mut self, width: Coord) {
+        self.layouts.push(Layout::Vbox(width));
+    }
+    pub fn vbox_end(&mut self) {
+        self.layouts.pop();
+    }
+    pub fn hbox_start(&mut self, height: Coord) {
+        self.layouts.push(Layout::Hbox(height));
+    }
+    pub fn hbox_end(&mut self) {
+        self.layouts.pop();
+    }
+    pub fn frame_start(&mut self, title: &str, width: Coord, height: Coord) {
+        let id = self.next_id();
+        let r = self.layout(Rect::new(0, 0, width, height.max(3)));
+        self.update_control(id, &r);
+        self.draw_frame(&r, title, ColorCode::Background);
+        self.layouts.push(Layout::Vbox(width));
+        self.cursor.y+=1;
+    }
+    pub fn frame_end(&mut self) {
+        self.layouts.pop();
+        self.cursor.y+=1;
+    }
+    pub fn label(&mut self, label: &str, align: TextAlign) {
+        let id = self.next_id();
+        let r = self.layout(Rect::new(0, 0, label.len() as Coord, 1));
+        self.update_control(id, &r);
+        self.draw_rect(&r, ColorCode::Background);
+        self.draw_text(&r, label, align, ColorCode::Text);
+    }
+    pub fn button(&mut self, label: &str, align: TextAlign) -> bool {
+        let id = self.next_id();
+        let r = self.layout(Rect::new(0, 0, label.len() as Coord, 1));
+        self.update_control(id, &r);
+        let focus = self.focus == id;
+        let hover = self.hover == id;
+        let pressed = focus && self.mouse_pressed == MOUSE_BUTTON_LEFT;
+        self.draw_rect(
+            &r,
+            if focus {
+                ColorCode::ButtonBackgroundFocus
+            } else if hover {
+                ColorCode::ButtonBackgroundHover
+            } else {
+                ColorCode::ButtonBackground
+            },
+        );
+        self.draw_text(&r, label, align, ColorCode::Text);
+        pressed
+    }
+
+    fn draw_frame(&mut self, r: &Rect, title: &str, col: ColorCode) {
+        self.commands
+            .push(Command::Frame(title.to_owned(), (*r).clone(), col));
+    }
+
+    fn draw_rect(&mut self, r: &Rect, col: ColorCode) {
+        self.commands.push(Command::Rect((*r).clone(), col));
+    }
+
+    fn draw_text(&mut self, r: &Rect, txt: &str, align: TextAlign, col: ColorCode) {
+        let mut p: Pos = r.into();
+        let truncated_txt: String;
+        let len = txt.len() as Coord;
+        match align {
+            TextAlign::Left => truncated_txt = txt[0..r.w.min(len) as usize].to_owned(),
+            TextAlign::Right => {
+                p.x = p.x + r.w - len;
+                if p.x < 0 {
+                    truncated_txt = txt[(-p.x) as usize..].to_owned();
+                    p.x = 0;
+                } else {
+                    truncated_txt = txt.to_owned();
+                }
+            }
+            TextAlign::Center => {
+                if len > r.w {
+                    let to_remove = (len - r.w) as usize;
+                    let start = (to_remove / 2) as usize;
+                    let end = (len as usize - (to_remove - start)) as usize;
+                    truncated_txt = txt[start..end].to_owned();
+                } else {
+                    truncated_txt = txt.to_owned();
+                }
+                p.x = p.x + r.w / 2 - len / 2;
+            }
+        };
+        self.commands.push(Command::Text(truncated_txt, p, col));
+    }
+
+    fn update_control(&mut self, id: usize, r: &Rect) {
+        let mouse_over = r.contains(self.mouse_pos);
+        let pressed = self.mouse_pressed != 0;
+        if mouse_over {
+            self.hover = id;
+            if pressed {
+                self.set_focus(id);
+            }
+        } else {
+            self.hover = 0;
+            if self.focus == id && pressed {
+                self.set_focus(0);
+            }
+        }
+    }
+
+    fn set_focus(&mut self, id: usize) {
+        self.focus = id;
+        self.updated_focus = true;
+    }
+
+    fn layout(&mut self, r: Rect) -> Rect {
+        let mut r = Rect::new(r.x + self.cursor.x, r.y + self.cursor.y, r.w, r.h);
+        match self.layouts.last() {
+            Some(Layout::Vbox(w)) => {
+                r.w = *w;
+                self.cursor.y += 1;
+            }
+            Some(Layout::Hbox(h)) => {
+                r.h = *h;
+                self.cursor.x += r.w;
+            }
+            None => (),
+        }
+        r
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate as ui;
+
+    const SCREEN_WIDTH: usize = 80;
+    const SCREEN_HEIGHT: usize = 25;
+    struct AsciiRenderer {
+        character: [[char; SCREEN_HEIGHT]; SCREEN_WIDTH],
+        background_color: [[ui::ColorCode; SCREEN_HEIGHT]; SCREEN_WIDTH],
+        foreground_color: [[ui::ColorCode; SCREEN_HEIGHT]; SCREEN_WIDTH],
+    }
+    impl AsciiRenderer {
+        pub fn new() -> Self {
+            Self {
+                character: [[' '; SCREEN_HEIGHT]; SCREEN_WIDTH],
+                background_color: [[ui::ColorCode::Background; SCREEN_HEIGHT]; SCREEN_WIDTH],
+                foreground_color: [[ui::ColorCode::Foreground; SCREEN_HEIGHT]; SCREEN_WIDTH],
+            }
+        }
+        pub fn assert(&self, t: &str, x: usize, y: usize) -> bool {
+            let mut cx = x;
+            for c in t.chars() {
+                if self.character[cx][y] != c {
+                    return false;
+                }
+                cx += 1;
+            }
+            true
+        }
+    }
+    impl ui::Renderer for AsciiRenderer {
+        fn rectangle(&mut self, rect: &ui::Rect, col: ui::ColorCode) {
+            for cx in rect.x as usize..(rect.x + rect.w) as usize {
+                for cy in rect.y as usize..(rect.y + rect.h) as usize {
+                    self.character[cx][cy] = ' ';
+                    self.background_color[cx][cy] = col;
+                }
+            }
+        }
+        fn text(&mut self, pos: ui::Pos, txt: &str, col: ui::ColorCode) {
+            let mut x = pos.x as usize;
+            let y = pos.y as usize;
+            for c in txt.chars() {
+                self.character[x][y] = c;
+                self.foreground_color[x][y] = col;
+                x += 1;
+            }
+        }
+        fn frame(&mut self, txt: &str, rect: &ui::Rect, col: ui::ColorCode) {
+            let rx = rect.x as usize;
+            let ry = rect.y as usize;
+            let rx2 = rx + rect.w as usize - 1;
+            let ry2 = ry + rect.h as usize - 1;
+            self.character[rx][ry] = '1';
+            self.character[rx2][ry] = '2';
+            self.character[rx][ry2] = '3';
+            self.character[rx2][ry2] = '4';
+            self.text(rect.into(), txt, col);
+        }
+    }
+
+    #[test]
+    fn test_button() {
+        let mut rend = AsciiRenderer::new();
+        let mut ctx = ui::Context::new();
+        ctx.begin();
+        ctx.button("test", ui::TextAlign::Left);
+        ctx.end();
+        ctx.render(&mut rend);
+        assert!(rend.assert("test", 0, 0));
+    }
+}
